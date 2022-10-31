23d9e943b3a2fd299552cde0015036ae203232da
Fabiele Hoerlle <fabihoerlle@gmail.com>
2022-10-31 15:40:44 -0300

    For Dart

---
+++ for.dart
@@ -0,0 +1,23 @@
+//import 'if.dart';
+
+main() {
+  List<String> listaProdutos = [
+    "Mimo Feliz",
+    "Box Te Amo",
+    "Vaso de Margarida",
+    "Rosas Vermelhas"
+  ];
+
+  for (int i = 0; i < listaProdutos.length; i++) {
+    print(listaProdutos[i]);
+  }
+
+  // for (int indice = 0; indice < listaProdutos.length; indice++) {
+  //   String nome = listaProdutos[indice];
+  //   print(nome);
+  // }
+
+  //for (int idade = 11; idade <= 60; idade++) {
+  //idadeComIf(idade);
+  //}
+}
