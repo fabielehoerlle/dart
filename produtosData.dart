b81e54cb1c91c86e1fc8cd7c0cdbaa7153db991d
Fabiele Hoerlle <fabihoerlle@gmail.com>
2022-11-23 17:05:19 -0300

    IF

--- produtosData.dart
+++ produtosData.dart
@@ -15,6 +15,9 @@ class ProductData {
     int reais = this.price.toInt();
     int centavos = ((this.price - reais) * 100).toInt();
     String formatado = "R\$ $reais,${centavos.toString().padRight(2, "0")}";
+    if (this.enabled == false) {
+      return "Produto Esgotado";
+    }
     return formatado;
   }
 }
@@ -180,6 +183,114 @@ List<ProductData> romanticProducts = [
     image: "assets/10.png",
     name: "Rustic Chic",
     price: 220.00,
+    enabled: false,
+  ),
+];
+
+List<ProductData> babyProducts = [
+  ProductData(
+    image: "assets/20.png",
+    name: "Box Welcome Baby",
+    price: 220.00,
+  ),
+  ProductData(
+    image: "assets/29.png",
+    name: "Box Baby Girl",
+    price: 199.00,
+  ),
+  ProductData(
+    image: "assets/27.png",
+    name: "Nana Menino",
+    price: 345.00,
+  ),
+  ProductData(
+    image: "assets/22.png",
+    name: "Nana Menina",
+    price: 345.00,
+  ),
+  ProductData(
+    image: "assets/28.png",
+    name: "Box Baby",
+    price: 199.00,
+  ),
+  ProductData(
+    image: "assets/23.png",
+    name: "É uma menina",
+    price: 119.00,
+  ),
+  ProductData(
+    image: "assets/26.png",
+    name: "É um menino",
+    price: 119.00,
+  ),
+  ProductData(
+    image: "assets/25.png",
+    name: "Pequena Fofura",
+    price: 175.00,
+  ),
+  ProductData(
+    image: "assets/21.png",
+    name: "Baby Urso",
+    price: 189.00,
+  ),
+  ProductData(
+    image: "assets/30.png",
+    name: "Box Girl Fofura",
+    price: 175.00,
+  ),
+  ProductData(
+    image: "assets/24.png",
+    name: "Bem Vinda Menina",
+    price: 200.00,
+    enabled: false,
+  ),
+];
+
+List<ProductData> birthdayProducts = [
+  ProductData(
+    image: "assets/31.png",
+    name: "La Belle",
+    price: 340.00,
+  ),
+  ProductData(
+    image: "assets/32.png",
+    name: "Parabéns",
+    price: 389.00,
+  ),
+  ProductData(
+    image: "assets/33.png",
+    name: "Mix Colors",
+    price: 190.00,
+  ),
+  ProductData(
+    image: "assets/34.png",
+    name: "Delícias da Manhã",
+    price: 295.00,
+  ),
+  ProductData(
+    image: "assets/35.png",
+    name: "Grande Festa",
+    price: 350.00,
+  ),
+  ProductData(
+    image: "assets/36.png",
+    name: "Cesta Parabéns",
+    price: 165.00,
+  ),
+  ProductData(
+    image: "assets/37.png",
+    name: "Happy Birthday",
+    price: 266.00,
+  ),
+  ProductData(
+    image: "assets/38.png",
+    name: "Mimo Colorido",
+    price: 79.00,
+  ),
+  ProductData(
+    image: "assets/39.png",
+    name: "Orquídea Birthday",
+    price: 184.00,
     enabled: false,
   ),
 ];
